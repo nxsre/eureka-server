ec96e3d20422634a623ecd9b03770e50d5ebca4f
soopsio <admin@soops.io>
2020-05-27 18:02:35 +0800

    add event notify

--- src/main/java/com/example/eureka/listener/EurekaStateChangeListener.java
+++ src/main/java/com/example/eureka/listener/EurekaStateChangeListener.java
@@ -1,9 +1,21 @@
 package com.example.eureka.listener;
 
 import com.netflix.appinfo.InstanceInfo;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.springframework.cloud.netflix.eureka.server.event.*;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import com.google.gson.Gson;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+
 /**
  * @Classname EurekaStateChangeListener
  * @Description 监听服务上下线
@@ -13,28 +25,61 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class EurekaStateChangeListener {
+    @Value("${eureka.notify.http.url}")
+    private String notifyUrl;
 
     @EventListener
-    public void listen(EurekaInstanceCanceledEvent eurekaInstanceCanceledEvent) {
-        System.out.println(eurekaInstanceCanceledEvent.getServerId() + "\t" + eurekaInstanceCanceledEvent.getAppName() + "服务下线");
+    public void listen(EurekaInstanceCanceledEvent event) {
+        Gson gson = new Gson();
+        String json = gson.toJson(event);
+        send(json);
+//        System.out.println(eurekaInstanceCanceledEvent.getServerId() + "\t" + eurekaInstanceCanceledEvent.getAppName() + "服务下线");
     }
 
     @EventListener
     public void listen(EurekaInstanceRegisteredEvent event) {
-        InstanceInfo instanceInfo = event.getInstanceInfo();
-        System.err.println(instanceInfo.getAppName() + " 进行注册 ");
+        Gson gson = new Gson();
+        String json = gson.toJson(event);
+        send(json);
+//        InstanceInfo instanceInfo = event.getInstanceInfo();
+//        System.err.println(event.getTimestamp() + " " + instanceInfo.getAppName() + " 进行注册 ");
     }
 
     @EventListener
     public void listen(EurekaInstanceRenewedEvent event) {
-        System.err.println(event.getServerId() + "\t" +event.getAppName() + " 服务进行续约 ");
+        Gson gson = new Gson();
+        String json = gson.toJson(event);
+        send(json);
+//        System.err.println(event.getTimestamp() + " " + event.getServerId() + "\t" + event.getAppName() + " 服务进行续约 ");
     }
+
     @EventListener
     public void listen(EurekaRegistryAvailableEvent event) {
-        System.err.println(" 注册中心启动 ");
+        Gson gson = new Gson();
+        String json = gson.toJson(event);
+        send(json);
+//        System.err.println(event.getTimestamp() + " " + " 注册中心启动 ");
     }
+
     @EventListener
     public void listen(EurekaServerStartedEvent event) {
-        System.err.println("Eureka Server 启动 ");
+        Gson gson = new Gson();
+        String json = gson.toJson(event);
+        send(json);
+//        System.err.println(event.getTimestamp() + " " + "Eureka Server 启动 ");
+    }
+
+    private void send(String msg) {
+        StringEntity requestEntity = new StringEntity(
+                msg,
+                ContentType.APPLICATION_JSON);
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost postMethod = new HttpPost(notifyUrl);
+            postMethod.setEntity(requestEntity);
+            HttpResponse rawResponse = httpClient.execute(postMethod);
+        } catch (IOException e) {
+            System.err.println(msg + " " + notifyUrl + "\n" + e.toString());
+        }
     }
 }
